1945c3f5d97cec62373f81937769e7b2b6e98a3b
Remco Brilstra <remco.brilstra@gmail.com>
2024-10-07 12:35:20 +0200

    very janky image gen implementation, lets generalize this before we merge

---
+++ src/Clients/ImageGen.Test/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LLMChain.BlackForestLabs;
+using Microsoft.Extensions.Configuration;
+using System.Net;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var config = LoadConfiguration();
+
+        FluxProvider provider = new FluxProvider(config["BlackForestLabs:ApiKey"]);
+
+        var request = new FluxProvider.ImageGenRequest()
+        {
+            prompt = "An image of a woman with fair skin and natural, wavy hair styled in soft curls around her face. She has defined eyebrows and light-colored eyes that draw attention. She's dressed in a slytherin hogwarts school outfit, big wizards robe waving behind her. she holds up a wand with a green glowing tip. the woman is standing on a cliff overlooking the great lake showing hogwarts castle on the other side of the lake.",
+            width = 1024,
+            height = 1024,
+            //steps = 40,
+            prompt_upsampling = true,
+            //seed = 42,
+            safety_tolerance = 6
+        };
+
+        var resp = await provider.QueueGenerateImage(request);
+
+        var result = await provider.GetResult(resp.id);
+        
+        while(result.Status != FluxProvider.ImageGenResult.StatusResponse.Ready)
+        {
+            result = await provider.GetResult(resp.id);
+        }
+        
+        var imgUrl = result.Result["sample"].ToString();
+
+        using (var client = new WebClient())
+        {
+            client.DownloadFile(imgUrl, $"{resp.id}.jpg");
+        }
+
+
+        Console.WriteLine("Bye, World!");
+
+
+
+        static IConfiguration LoadConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile("appsettings.local.json", optional: true, reloadOnChange: true);
+
+            return builder.Build();
+        }
+    }
+}
---
+++ src/LLMChain.BlackForestLabs/FluxProvider.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Threading.Tasks;
+//using Newtonsoft.Json;
+
+namespace LLMChain.BlackForestLabs
+{
+    public class FluxProvider
+    {
+        private string ApiKey { get; set; }
+
+        private const string API_ENDPOINT = "https://api.bfl.ml/v1/";
+
+        private string ApiEndpointRoot { get; set; }
+
+        JsonSerializerOptions jsSerializerOption = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault,
+            Converters ={
+                new JsonStringEnumConverter(JsonNamingPolicy.CamelCase)
+            },
+
+        };
+
+        public class ImageGenRequest
+        {
+            public string prompt { get; set; }
+            public int? width { get; set; }
+            public int? height { get; set; }
+            public int? steps { get; set; }
+            public bool prompt_upsampling { get; set; }
+            public int? seed { get; set; } = null;
+            public int safety_tolerance { get; set; }
+        }
+
+        public class ImageGenResponse
+        {
+            public string id { get; set; }
+        }
+
+            public FluxProvider(string apiKey, string apiRoot = API_ENDPOINT)
+        {
+            ApiKey = apiKey;
+            ApiEndpointRoot = apiRoot;
+        }
+
+        public class ImageGenResult
+        {
+            /// <summary>
+            /// Task id for retrieving result
+            /// </summary>
+            [JsonPropertyName("id")]
+            public string Id { get; set; }
+
+            [JsonPropertyName("result")]
+            public Dictionary<string, object> Result { get; set; }
+
+            [JsonPropertyName("status")]
+            public StatusResponse Status { get; set; }
+
+            public enum StatusResponse { ContentModerated, Error, Pending, Ready, RequestModerated, TaskNotFound };
+        }
+
+
+        public async Task<ImageGenResult> GetResult(string id)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"{ApiEndpointRoot}get_result?id={id}"),
+                Headers =
+                {
+                    { "X-Key", ApiKey },
+                },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<ImageGenResult>(body, jsSerializerOption);
+            }
+
+            return null;
+        }
+
+        public async Task<ImageGenResponse> QueueGenerateImage(ImageGenRequest requestData, string model = "flux-pro-1.1")
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri($"{ApiEndpointRoot}{model}"),
+                Headers =
+                {
+                    { "X-Key", ApiKey },
+                },
+                Content = new StringContent(JsonSerializer.Serialize(requestData, jsSerializerOption), Encoding.UTF8, "application/json"),
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+
+                return JsonSerializer.Deserialize<ImageGenResponse>(body, jsSerializerOption);
+
+            }
+
+            return null;
+        }
+
+    }
+}
